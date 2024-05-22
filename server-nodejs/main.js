a355932eb3f52a2eee76d9f9d585344081f13ed3
1anhphanle <anhphanle8818@gmail.com>
2024-05-22 10:52:38 +0700

    update server node js

---
+++ server-nodejs/main.js
@@ -0,0 +1,14 @@
+//Đây là module có sẵn để tạo server nodejs
+const http = require('http')
+const PORT = 4000
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-type', 'application/json')
+    res.end(`{"messege": "Hello World!"}`)
+})
+
+// console.log(http)
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+}) 
