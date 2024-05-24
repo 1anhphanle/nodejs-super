986604e2cdf0a5ac6601ef6edbd0f4f869f77da5
1anhphanle <anhphanle8818@gmail.com>
2024-05-24 22:18:31 +0700

    .

--- error-handling/src/index.ts
+++ error-handling/src/index.ts
@@ -1,19 +1,17 @@
-// lỗi typescript
-// interface User {
-//   name: string
-//   age: number
-// }
-// const render = (user: User) => {
-//   console.log(user)
-// }
-// const user = { name: 'Anh Phan Le', age: 20 } // phải thêm age vào trong User
-// render(user)
+import express from 'express'
+const app = express()
+const port = 3000
 
-//lỗi eslint
-// const profile: any = {
-//   name: 'Duc'
-// }
-
-const profile: { name: string } = {
-  name: 'Duc'
+//vd xu ly ham sum lam chuong trinh bi loi
+const sum = (obj: { a: number; b: number }) => {
+  return obj.a + obj.b
 }
+
+app.get('/', (req, res) => {
+  const value = sum({ a: 1, b: 2 }) //cu phap sai: sum({null as any})
+  res.send(`Anh Phan Le, ${value}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
